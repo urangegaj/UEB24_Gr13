2077f0c0f34250675112358afc9671c0ebf11fd0
Rinesa Saraçi <rinesasaraci1@gmail.com>
2025-05-26 21:41:51 +0200

    .

--- bin.php
+++ bin.php
@@ -235,9 +235,9 @@ body {
                     </div>
                     <nav>
                         <ul class="nav-links">
-                            <li><a href="./index.html">Home</a></li>
+                            <li><a href="./index.php">Home</a></li>
                             <li><a href="/Products.php">Products</a></li>
-                            <li><a href="./About.html">About</a></li>
+                            <li><a href="./About.php">About</a></li>
                             <li><a href="./Contact.html">Contact</a></li>
                         </ul>
                        
